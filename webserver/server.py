fc0ca71aefa936347e664cdcfc17ff24e5cd0302
mbishopn <145691784+mbishopn@users.noreply.github.com>
2024-04-16 08:56:04 -0400

    semi-final
    now both models deliver their predictions in the website.
    
    still to do:
     css, and a better interface to enter data
    readme.me

--- webserver/server.py
+++ webserver/server.py
@@ -21,8 +21,7 @@ cvarDesc=heartDisease.variables
 
 jl_filedir = Path("./trained_models")
 jl_filedir.mkdir(parents=True,exist_ok=True)
-jl_filepath=jl_filedir / 'reg_obesity.joblib'
-regressor=joblib.load(jl_filepath)
+
 
 
 
@@ -38,38 +37,30 @@ def welcome():
 def about():
         return render_template('about.html')
 
-@app.route("/regressor")
+@app.route("/regressor", methods=['GET','POST']) # ------------------------- ROUTE TO LOAD REGRESSION MODEL
 def regressor():
-        return render_template('regressor.html')
+        jl_filepath=jl_filedir / 'reg_obesity.joblib'
+        regressor=joblib.load(jl_filepath)
+        data=""
+        if request.method=='POST':
+                arr=[]
+                data=pd.DataFrame(columns=['Age','Height','FCVC','NCP','CH2O','FAF','TUE','Gender','family_history_with_overweight','FAVC','CAEC','SCC','CALC','MTRANS','Height_na'])
+                for x in data.columns:
+                  arr.append(request.form[x])
+                data=regressor.predict([arr])
+        return render_template('regressor.html', pred=data)
 
-@app.route("/classifier", methods=['GET','POST'])
+@app.route("/classifier", methods=['GET','POST']) # ---------------------------- ROUTE TO LOAD CLASSIFICATION MODEL
 def classifier():
         jl_filepath=jl_filedir / 'class_heart.joblib'
         classifier=joblib.load(jl_filepath)
-
+        data=""
         if request.method=='POST':
                 arr=[]
-                
                 data=pd.DataFrame(columns=['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','ca_na','thal_na'])
-                arr.append(request.form['age'])
-                arr.append(request.form['sex'])
-                arr.append(request.form['cp'])
-                arr.append(request.form['trest'])
-                arr.append(request.form['chol'])
-                arr.append(request.form['fbs'])
-                arr.append(request.form['restecg'])
-                arr.append(request.form['thalach'])
-                arr.append(request.form['exang'])
-                arr.append(request.form['oldpeak'])
-                arr.append(request.form['slope'])
-                arr.append(request.form['ca'])
-                arr.append(request.form['thal'])
-                arr.append(0)
-                arr.append(0)
+                for x in data.columns:
+                  arr.append(request.form[x])
                 data=classifier.predict([arr])
-                print(data)
-        else:
-                data="Fill the form"
         return render_template('classifier.html', pred=data)
 
 @app.route("/data/<model>")
@@ -78,14 +69,12 @@ def data(model):
                 con=sqlite3.connect('./database/models.db')
                 cur1=con.cursor();cur2=con.cursor()
                 sample=rraw.sample(5)
-                # raw=cur1.execute("select * from rawData order by random() limit 5")
                 clean=cur2.execute("select * from reg_clean_data order by random() limit 5")
                 return render_template('data_reg.html',raw=sample,clean=clean,varDesc=rvarDesc)
         if(model=='classification'):
                 con=sqlite3.connect('./database/models.db')
                 cur1=con.cursor();cur2=con.cursor()
                 sample=craw.sample(5)
-                # raw=cur1.execute("select * from rawData order by random() limit 5")
                 clean=cur2.execute("select * from class_clean_data order by random() limit 5")
                 return render_template('data_class.html',raw=sample,clean=clean,varDesc=cvarDesc)
 
